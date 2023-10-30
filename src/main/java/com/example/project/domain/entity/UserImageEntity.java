2454e94e40419f914b8f4fe0040ae8414aa72ca8
sengjun0624 <sengjun0624@naver.com>
2023-10-30 20:37:39 +0900

    feat(UserImageEntity): add UserImageEntity

--- src/main/java/com/example/project/domain/entity/UserImageEntity.java
+++ src/main/java/com/example/project/domain/entity/UserImageEntity.java
@@ -6,19 +6,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "PresentImage")
-public class PresentImageEntity extends BaseEntity {
+@Table(name = "UserImage")
+public class UserImageEntity extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column private String src;
 
-    @ManyToOne
-    @JoinColumn(name = "PresentId", referencedColumnName = "id")
-    private PresentEntity present;
+    @OneToOne
+    @JoinColumn(name = "UserId", referencedColumnName = "id")
+    private UserEntity user;
+
+    public UserImageEntity() {}
+
+    // 생성자, 게터, 세터, 기타 메서드 생략
 }
